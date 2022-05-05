23fd5a43fd2552ab456c11a72f50699466cadff2
oLsen <scr4t@hotmail.de>
2022-05-05 22:11:34 +0200

    Test TODOs

---
+++ test/canvas/MovementCalculatorImpl.test.ts
@@ -0,0 +1,12 @@
+import {MovementCalculatorImpl} from "../../ts/canvas/impl/MovementCalculatorImpl";
+
+const chai = require('chai');
+const mocha = require('mocha');
+
+const radius = 10;
+const calculator = new MovementCalculatorImpl(radius);
+
+mocha.describe('Canvas Movement Calc', () => {
+    mocha.it('TODO', () => {
+    });
+});
---
+++ test/canvas/VelocityCalculatorImpl.test.ts
@@ -0,0 +1,11 @@
+import {VelocityCalculatorImpl} from "../../ts/canvas/impl/VelocityCalculatorImpl";
+
+const chai = require('chai');
+const mocha = require('mocha');
+
+const calculator = new VelocityCalculatorImpl();
+
+mocha.describe('Canvas Velocity Calc', () => {
+    mocha.it('TODO', () => {
+    });
+});
