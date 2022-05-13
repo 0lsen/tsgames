0d17105caa18f64f0babb0e66fca6be8b5867642
oLsen <scr4t@hotmail.de>
2022-05-13 17:06:09 +0200

    snake beautification

--- ts/snake/App.ts
+++ ts/snake/App.ts
@@ -8,7 +8,12 @@ export class App extends BaseApp {
     private readonly CLASS_LOST = 'lost';
     private readonly CLASS_HEAD = 'head';
     private readonly CLASS_BODY = 'body';
+    private readonly CLASS_TAIL = 'tail';
     private readonly CLASS_FOOD = 'food';
+    private readonly CLASS_NORTH = 'north';
+    private readonly CLASS_EAST = 'east';
+    private readonly CLASS_SOUTH = 'south';
+    private readonly CLASS_WEST = 'west';
 
     private readonly $body = $('body');
     private readonly $board = $('#board');
@@ -42,10 +47,18 @@ export class App extends BaseApp {
         for (let i = 0; i < this.settings.height; i++) {
             let $row = $('<div></div>');
             for (let j = 0; j < this.settings.width; j++) {
-                $row.append('<div></div>');
+                $row.append(this.cell);
             }
             this.$board.append($row);
         }
+    }
+
+    private cell() : JQuery {
+        let $cell = $('<div></div>');
+        for (let i = 0; i < 9; i++) {
+            $cell.append('<div></div>');
+        }
+        return $cell;
     }
 
     private changeDirection(direction : Direction) : void {
@@ -66,6 +79,7 @@ export class App extends BaseApp {
         this.$board.removeClass(this.CLASS_LOST);
         this.$board.find('div').removeClass(this.CLASS_HEAD);
         this.$board.find('div').removeClass(this.CLASS_BODY);
+        this.$board.find('div').removeClass(this.CLASS_TAIL);
         this.placeFood();
         this.placeHead();
         this.settings.speed = this.getSpeed();
@@ -84,22 +98,29 @@ export class App extends BaseApp {
         this.directionChange = false;
         let newHeadPosition = new Coord(newX, newY);
 
-        let foodConsumed = newHeadPosition.equals(this.foodPosition);
-        if (foodConsumed) {
-            this.placeFood();
-        } else {
-            if (this.bodyPositions.length) this.removeClass(this.bodyPositions[0], this.CLASS_BODY);
-            this.bodyPositions.shift();
-        }
-        
         if (this.collision(newHeadPosition)) {
             this.$board.addClass(this.CLASS_LOST);
             this.isRunning = false;
         } else {
+            let foodConsumed = newHeadPosition.equals(this.foodPosition);
+            if (foodConsumed) {
+                this.placeFood();
+            } else {
+                if (this.bodyPositions.length > 1) {
+                    this.removeClass(this.bodyPositions[1], this.CLASS_BODY);
+                    this.addClass(this.bodyPositions[1], this.CLASS_TAIL);
+                }
+                this.clearCell(this.bodyPositions[0]);
+                this.bodyPositions.shift();
+            }
             this.addClass(newHeadPosition, this.CLASS_HEAD);
-            if (this.bodyPositions.length) this.addClass(this.headPosition, this.CLASS_BODY);
+            if (this.bodyPositions.length) {
+                this.addClass(this.headPosition, this.bodyPositions.length > 1 ? this.CLASS_BODY : this.CLASS_TAIL);
+            }
             this.removeClass(this.headPosition, this.CLASS_HEAD);
             this.headPosition = newHeadPosition;
+            this.setHeadOrientation();
+            this.setTailOrientation();
             setTimeout(() => this.slither(), 1200/this.settings.speed);
         }
     }
@@ -121,12 +142,54 @@ export class App extends BaseApp {
         this.addClass(this.headPosition, this.CLASS_HEAD);
     }
 
+    private getCell(position: Coord) : JQuery {
+        return this.$board.find(' > div:nth-child('+(position.y+1)+') > div:nth-child('+(position.x+1)+')');
+    }
+
     private addClass(position : Coord, clazz : string) : void {
-        this.$board.find(' > div:nth-child('+(position.y+1)+') > div:nth-child('+(position.x+1)+')').addClass(clazz);
+        this.getCell(position).addClass(clazz);
     }
 
     private removeClass(position : Coord, clazz : string) : void {
-        this.$board.find(' > div:nth-child('+(position.y+1)+') > div:nth-child('+(position.x+1)+')').removeClass(clazz);
+        this.getCell(position).removeClass(clazz);
+    }
+
+    private clearCell(position : Coord) : void {
+        this.getCell(position).removeClass();
+    }
+
+    private setHeadOrientation() : void {
+        this.removeDirections(this.headPosition);
+        if (this.bodyPositions.length) {
+            let neck = this.bodyPositions.at(-1);
+            if (neck.x != this.headPosition.x) {
+                this.addClass(neck, neck.x > this.headPosition.x ? this.CLASS_WEST : this.CLASS_EAST);
+                this.addClass(this.headPosition, neck.x > this.headPosition.x ? this.CLASS_EAST : this.CLASS_WEST);
+            } else {
+                this.addClass(neck, neck.y > this.headPosition.y ? this.CLASS_NORTH : this.CLASS_SOUTH);
+                this.addClass(this.headPosition, neck.y > this.headPosition.y ? this.CLASS_SOUTH : this.CLASS_NORTH);
+            }
+        }
+    }
+
+    private setTailOrientation() : void {
+        if (this.bodyPositions.length) {
+            let tail = this.bodyPositions[0];
+            let torso = this.bodyPositions.length > 1 ? this.bodyPositions[1] : this.headPosition;
+            this.removeDirections(tail);
+            if (tail.x != torso.x) {
+                this.addClass(tail, torso.x > tail.x ? this.CLASS_EAST : this.CLASS_WEST);
+            } else {
+                this.addClass(tail, torso.y > tail.y ? this.CLASS_SOUTH : this.CLASS_NORTH);
+            }
+        }
+    }
+
+    private removeDirections(position : Coord) : void {
+        this.removeClass(position, this.CLASS_NORTH);
+        this.removeClass(position, this.CLASS_EAST);
+        this.removeClass(position, this.CLASS_SOUTH);
+        this.removeClass(position, this.CLASS_WEST);
     }
 
     private foodPositionValid() : boolean {
