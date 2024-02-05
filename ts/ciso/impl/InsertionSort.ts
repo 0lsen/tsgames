ae1b68cc7655c3a5010366bbad9e538139a197fa
oLsen <scr4t@hotmail.de>
2024-02-05 23:02:54 +0100

    ciso: merge sort major optimisation

--- ts/ciso/impl/InsertionSort.ts
+++ ts/ciso/impl/InsertionSort.ts
@@ -4,17 +4,20 @@ import {AbstractSort} from "./AbstractSort";
 export class InsertionSort extends AbstractSort implements Sort {
 
     private index : number;
+    private readonly isMergeSort : boolean;
+    private searchStartIndex : number = 0;
 
     constructor(values: number[], index: number = 1) {
         super(values);
         this.index = index;
+        this.isMergeSort = index > 1;
     }
 
     iterate(): void {
         const value = this.values[this.index];
         this._movingFrom = undefined;
         this._movingTo = undefined;
-        for (let i = 0; i < this.index; i++) {
+        for (let i = this.searchStartIndex; i < this.index; i++) {
             if (this.compare(value, this.values[i])) {
                 this._movingFrom = this.index;
                 this._movingTo = i;
@@ -22,6 +25,9 @@ export class InsertionSort extends AbstractSort implements Sort {
                     .concat(value)
                     .concat(this.values.slice(i, this.index))
                     .concat(this.values.slice(Math.min(this.values.length, this.index+1)));
+                if (this.isMergeSort) {
+                    this.searchStartIndex = i;
+                }
                 break;
             }
         }
